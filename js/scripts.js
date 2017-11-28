07f19610b607758940c1f4e1e5c41bd3bf649189
Glen Buck and Ross Fletcher <glen@>
2017-11-28 14:55:33 -0800

    create site for flashcards using bootstrap,css,jquery and added styling

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+// https://jsfiddle.net/james2doyle/qsQun/
+
+$(document).ready(function() {
+  $(".javascript").click(function() {
+    $(".comments").fadeToggle();
+  });
+
+  $(".operators").click(function() {
+    $(".comments2").fadeToggle();
+  });
+
+  $(".variables").click(function() {
+    $(".comments3").fadeToggle();
+  });
+
+  $(".vnc").click(function() {
+    $(".comments4").fadeToggle();
+  });
+
+  $(".functions").click(function() {
+    $(".comments5").fadeToggle();
+  });
+
+  $(".methods").click(function() {
+    $(".comments6").fadeToggle();
+  });
+
+  $(".arguments").click(function() {
+    $(".comments7").fadeToggle();
+  });
+
+  $(".parameters").click(function() {
+    $(".comments8").fadeToggle();
+  });
+
+  $(".return").click(function() {
+    $(".comments9").fadeToggle();
+  });
+
+  $(".changeMethod").click(function() {
+    $(".comments10").fadeToggle();
+  });
+
+  $(".strings").click(function() {
+    $(".comments11").fadeToggle();
+  });
+
+  $(".boolean").click(function() {
+    $(".comments12").fadeToggle();
+  });
+
+  $(".undefined").click(function() {
+    $(".comments13").fadeToggle();
+  });
+
+  $(".escape").click(function() {
+    $(".comments14").fadeToggle();
+  });
+
+  $(".alert1").click(function() {
+    $(".comments15").fadeToggle();
+  });
+
+  $(".comments1").click(function() {
+    $(".comments16").fadeToggle();
+  });
+
+  $(".jquery").click(function() {
+    $(".comments17").fadeToggle();
+  });
+
+  $(".attributes").click(function() {
+    $(".comments18").fadeToggle();
+  });
+
+
+});
